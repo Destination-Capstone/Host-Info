f560060346998cbf35f7153679f120b0b36014e0
aflatness <aflatness@gmail.com>
2021-03-03 08:10:24 -0800

    Redirect base path to first listing

--- Client/components/app.jsx
+++ Client/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Location from './Location.jsx';
 import Host from './HostInfo.jsx';
 import Know from './ToKnow.jsx';
@@ -7,6 +7,11 @@ import Know from './ToKnow.jsx';
 const App = () => (
   <HashRouter>
     <Switch>
+      <Route
+        exact
+        path='/'
+        render={() => <Redirect to='/listing/1' />}
+      />
       <Route path='/listing/:id'>
         <div>
           <Location />
